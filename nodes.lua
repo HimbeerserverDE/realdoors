5384c1eb154d75ac33e88c7e0074a5d02f0663a0
HimbeerserverDE <lpaschek@icloud.com>
2020-10-03 18:13:37 +0200

    Add more kinds of doors

--- nodes.lua
+++ nodes.lua
@@ -12,4 +12,109 @@ realdoors.register_electronic({
 	tex_inv = "doors_item_wood.png",
 })
 
+realdoors.register_mechanical({
+	name = "barn1",
+	desc = "Barn door 1",
+	tex = "doors_door_barn1.png",
+	tex_inv = "doors_item_barn1.png",
+})
+
+realdoors.register_mechanical({
+	name = "barn2",
+	desc = "Barn door 2",
+	tex = "doors_door_barn2.png",
+	tex_inv = "doors_item_barn2.png",
+})
+
+realdoors.register_mechanical({
+	name = "castle1",
+	desc = "Castle door 1",
+	tex = "door_door_castle1.png",
+	tex_inv = "doors_item_castle1.png",
+})
+
+realdoors.register_mechanical({
+	name = "castle2",
+	desc = "Castle door 2",
+	tex = "door_door_castle2.png",
+	tex_inv = "doors_item_castle2.png",
+})
+
+realdoors.register_mechanical({
+	name = "cottage1",
+	desc = "Cottage door 1",
+	tex = "doors_door_cottage1.png",
+	tex_inv = "doors_item_cottage1.png",
+})
+
+realdoors.register_mechanical({
+	name = "cottage2",
+	desc = "Cottage door 2",
+	tex = "doors_door_cottage2.png",
+	tex_inv = "doors_item_cottage2.png",
+})
+
+realdoors.register_mechanical({
+	name = "french",
+	desc = "French door",
+	tex = "doors_door_french.png",
+	tex_inv = "doors_item_french.png",
+})
+
+realdoors.register_mechanical({
+	name = "japanese",
+	desc = "Japanese door",
+	tex = "doors_door_japanese.png",
+	tex_inv = "doors_item_japanese.png",
+})
+
+realdoors.register_mechanical({
+	name = "mansion1",
+	desc = "Mansion door 1",
+	tex = "doors_door_mansion1.png",
+	tex_inv = "doors_item_mansion1.png",
+})
+
+realdoors.register_mechanical({
+	name = "mansion2",
+	desc = "Mansion door 2",
+	tex = "doors_door_mansion2.png",
+	tex_inv = "doors_item_mansion2.png",
+})
+
+realdoors.register_electronic({
+	name = "steelglass1",
+	desc = "Steel glass door 1",
+	tex = "doors_door_steelglass1.png",
+	tex_inv = "doors_item_steelglass2.png",
+})
+
+realdoors.register_electronic({
+	name = "steelglass2",
+	desc = "Steel glass door 2",
+	tex = "doors_door_steelglass2.png",
+	tex_inv = "doors_item_steelglass2.png",
+})
+
+realdoors.register_electronic({
+	name = "woodglass1",
+	desc = "Wood glass door 1",
+	tex = "doors_door_woodglass1.png",
+	tex_inv = "doors_item_woodglass1.png",
+})
+
+realdoors.register_electronic({
+	name = "woodglass2",
+	desc = "Wood glass door 2",
+	tex = "doors_door_woodglass2.png",
+	tex_inv = "doors_item_woodglass2.png",
+})
+
+realdoors.register_mechanical({
+	name = "prison",
+	desc = "Prison door",
+	tex = "xdecor_prison_door.png",
+	tex_inv = "xdecor_prison_door_inv.png",
+})
+
 minetest.log("verbose", "[realdoors] Loaded nodes.lua v" .. realdoors.v)
